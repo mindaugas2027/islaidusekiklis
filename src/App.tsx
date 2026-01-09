f27caaf743f9ad16b5041b85c4828babc7429029
[dyad] <gotlimitedlook@gmail.com>
2026-01-09 14:04:53 +0200

    [dyad] wrote 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,12 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter 
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true
+          }}
+        >
           <Routes>
             <Route path="/login" element={!session ? <Login /> : <Navigate to="/" />} />
             <Route path="/" element={session ? <Index /> : <Navigate to="/login" />} />
